f90e612d11166010081fc2c6b645a557fa24a034
Egor Lukoyanov <52897993+Egordoesprogramming@users.noreply.github.com>
2020-11-24 20:55:09 -0500

    Added a tv screen and Cars AI

---
+++ DistractedDrivers/Assets/Scripts/MoviePlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class MoviePlayer : MonoBehaviour
+{
+    public MovieTexture movie;
+
+    void Start()
+    {
+        GetComponent<RawImage>().texture = movie as MovieTexture;
+
+        movie.Play();
+
+    }
+    void Update()
+    {
+        
+    }
+}
+
