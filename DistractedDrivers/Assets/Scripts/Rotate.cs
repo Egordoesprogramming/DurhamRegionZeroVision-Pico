f13165bdc462fd55c9845ddb2aeb0fe387d3d1ba
Egor Lukoyanov <52897993+Egordoesprogramming@users.noreply.github.com>
2020-11-26 01:03:18 -0500

    Added Road AI and all Tv screens

---
+++ DistractedDrivers/Assets/Scripts/Rotate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+   
+   void Start()
+    {
+       
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       
+        var speed = 10;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+        }
+
+      
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * speed * Time.deltaTime);
+        }
+
+       
+    }
+}
